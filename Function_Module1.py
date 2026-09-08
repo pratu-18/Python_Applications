af5ae34adce0336039cd2e8b802e277684fc8cf0
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-08 13:50:18 +0530

    Add files via upload

---
+++ Function_Module1.py
@@ -0,0 +1,18 @@
+#ERROR solved in this code
+import Marvellous2
+
+
+
+
+def main():
+    print("enter 1st no ")
+    value1=int(input())
+    print("enter 2nd no ")
+    value2=int(input())
+
+    ret=Marvellous2.Addition(value1,value2)
+    print("addition is ",ret)
+
+
+if __name__=="__main__":#starter,code starts from here
+    main() 
