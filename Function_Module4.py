b45427480a6750f2d2867d1540884e1e0bcd6c0e
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-08 13:51:58 +0530

    Add files via upload

---
+++ Function_Module4.py
@@ -0,0 +1,21 @@
+
+from Marvellous2 import *
+
+
+
+
+def main():
+    print("enter 1st no ")
+    value1=int(input())
+    print("enter 2nd no ")
+    value2=int(input())
+
+    ret=Addition(value1,value2)
+   
+    print("addition is ",ret)
+    ret=Substraction(value1,value2)
+    print("Substraction is ",ret)
+
+
+if __name__=="__main__":#starter,code starts from here
+    main() 
