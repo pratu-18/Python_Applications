b829c00a4ba69e89321bb9a54d32c57342028b4c
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-09 14:56:14 +0530

    Add files via upload

---
+++ ListTravel.py
@@ -0,0 +1,11 @@
+
+def main():
+   maraks=[78,90,45,65]
+
+   for no in maraks:#for each loop 
+       print(no)#marks nachya list cha yek yek variable or data kada ani no mde taka ani print kra 
+
+
+
+if __name__=="__main__":
+    main()
