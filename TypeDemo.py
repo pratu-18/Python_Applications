6571cba0ef95f747ce5c4162dd27c45a24543fb8
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-11 10:59:05 +0530

    Add files via upload

---
+++ TypeDemo.py
@@ -0,0 +1,25 @@
+
+#data type 1= NUMBER
+no=11 #int
+marks=90.78 #float
+value=8+5j #complex j is imagenary constance 
+
+print(value)
+print(type(no))
+print(type(marks))
+print(type(value))
+
+print("\n")
+
+#Bolean data type
+
+gender=True
+print(gender)
+print(type(gender))
+
+print("\n")
+
+#None data type
+dem=None
+print(dem)
+print(type(dem))
