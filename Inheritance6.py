d635f79e99a9f2c1de81acb41dbc4f0cc5459610
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-15 19:34:34 +0530

    Add files via upload

---
+++ Inheritance6.py
@@ -0,0 +1,21 @@
+#multiple inheritance
+class Base1:
+        
+    def fun(self):
+        print("Inside base1 fun")
+        
+class Base2:
+        
+    def gun(self):
+        print("Inside base2 gun")
+ 
+
+class Derived(Base1,Base2):
+    def sun(self):
+        print("Inside derived sun")
+    
+    
+bobj=Derived()
+bobj.fun()#child can access this
+bobj.gun()
+bobj.sun()
