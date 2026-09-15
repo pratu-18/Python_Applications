346c569e47962a541a6816583bd1f4c30b0845f6
pratu-18 <163864223+pratu-18@users.noreply.github.com>
2026-09-15 19:33:09 +0530

    Add files via upload

---
+++ Inheritance3.py
@@ -0,0 +1,14 @@
+class Base:
+    def __init__(self):
+        print("Inside base constructor ")
+        
+    
+
+
+class Derived(Base):
+    def __init__(self):
+        super().__init__()
+        print("Inside derived constructor")
+    
+    
+bobj=Derived()
